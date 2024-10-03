529ca27096b2d23958ca19d573275d0a8917d68b
jankovidakovic <janko@tensorpix.ai>
2024-10-03 12:05:06 +0200

    Add harpoon for buffer navigation

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,29 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+
+		harpoon:setup()
+
+		-- TODO -- filter out nvim_tree from the list
+
+		-- add current buffer to the list
+		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+
+		-- how to remove from list tho
+
+		-- list all buffers
+		vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+		vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+		vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+		vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<A-h>", function() harpoon:list():prev() end)
+		vim.keymap.set("n", "<A-l>", function() harpoon:list():next() end)
+	end
+}
