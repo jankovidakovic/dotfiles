d217b37b143ebdf9b5562c2035c7790e4f99b662
jankovidakovic <janko@tensorpix.ai>
2024-10-03 12:04:02 +0200

    Remove fixed comment

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -7,31 +7,30 @@ vim.opt.termguicolors = true
 
 -- as per https://github.com/nvim-tree/nvim-tree.lua?tab=readme-ov-file#custom-mappings, this is the way to change the mappings
 local function my_on_attach(bufnr)
-  local api = require "nvim-tree.api"
+	local api = require "nvim-tree.api"
 
-  -- utility function to provide the default mapping options
-  -- TODO -- optionally refactor this into a more general utility function (altho thats really not needed)
-  local function opts(desc)
-    return { 
-		desc = "nvim-tree: " .. desc, 
-		--  when we only attach a mapping to a buffer, then sometimes nvimtree attaches to itself
-		--  and then the mappings dont work in other buffers.
-		--  Without setting the buffer, mappings are on nvim-level and should work?
-		--  buffer = bufnr, 
-		noremap = true, 
-		silent = true, 
-		nowait = true 
-	}	
-  end
+	-- utility function to provide the default mapping options
+	-- TODO -- optionally refactor this into a more general utility function (altho thats really not needed)
+	local function opts(desc)
+		return {
+			desc = "nvim-tree: " .. desc,
+			--  when we only attach a mapping to a buffer, then sometimes nvimtree attaches to itself
+			--  and then the mappings dont work in other buffers.
+			--  Without setting the buffer, mappings are on nvim-level and should work?
+			--  buffer = bufnr,
+			noremap = true,
+			silent = true,
+			nowait = true
+		}
+	end
 
-  -- default mappings
-  api.config.mappings.default_on_attach(bufnr)
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
 
-  -- custom mappings
-  vim.keymap.set("n", "<A-1>", api.tree.toggle, opts("Toggle"))
+	-- custom mappings
+	vim.keymap.set("n", "<A-1>", api.tree.toggle, opts("Toggle"))
 
-  vim.keymap.set("n", "<leader>e", api.tree.focus, opts("Focus"))
-  -- TODO -- why are these not working? they should kinda be working, no?
+	vim.keymap.set("n", "<leader>e", api.tree.focus, opts("Focus"))
 end
 
 return {
