eaf41f431a65dcb4841f6e1e812ea8472868d2c5
jankovidakovic <janko@tensorpix.ai>
2024-10-22 11:05:50 +0200

    [nvim] Add new mappings for commenting

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -30,3 +30,12 @@ vim.keymap.set('n', '<leader>h', '<C-w><C-h>', { desc = 'Move focus to the left 
 vim.keymap.set('n', '<leader>l', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<leader>j', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<leader>k', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+local comment = function(comment_keymap)
+	return function()
+		vim.cmd.norm(comment_keymap)
+	end
+end
+
+vim.keymap.set("v", "<leader>cc", comment("gc"))
+vim.keymap.set("n", "<leader>cc", comment("gcc"))
