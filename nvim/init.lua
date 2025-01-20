c88db7ffa330f84ee1226717e32a86d758f27a1a
jvidakovic <janko.vidakovic@fer.hr>
2025-01-20 23:37:57 +0100

    [nvim] Disable copilot

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,7 +10,7 @@ require("config.autocmds")
 require("config.lazy") -- this kinda requires and installs all the plugins
 
 -- sets up the copilot
-require("config.copilot")
+-- require("config.copilot")
 
 -- okay lets set up a colorscheme next
 vim.o.background = "dark"
--- nvim/lua/config/copilot.lua
+++
@@ -1,6 +0,0 @@
-vim.g.copilot_filetypes = { xml = false, markdown = false }
-vim.cmd [[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
-vim.cmd [[imap <silent><script><expr> <C-a> copilot#Accept("\<CR>")]]
-vim.g.copilot_on_tab_map = true
-
-vim.api.nvim_set_keymap("i", "C-J", 'copilot#Accept("<CR>")', { silent = true, expr = true })
