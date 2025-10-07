422f49d2c858cb93823b2053bfa6cec10f0833da
jankovidakovic <janko@tensorpix.ai>
2025-10-07 15:58:00 +0200

    [nvim] [lsp] Re-add basedpyright and enable blacklisting lsp servers

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,4 +1,4 @@
-lsp_dependencies = {
+local lsp_dependencies = {
 	-- Automatically install LSPs and related tools to stdpath for Neovim
 	{ 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
 	'williamboman/mason-lspconfig.nvim',
@@ -10,6 +10,10 @@ lsp_dependencies = {
 
 	-- Allows extra capabilities provided by nvim-cmp
 	'hrsh7th/cmp-nvim-lsp',
+}
+
+local blacklist_lsp_servers = {
+	"basedpyright"
 }
 
 return {
@@ -28,8 +32,18 @@ return {
 		local servers = require("plugins.lsp.servers")
 
 		local ensure_installed = vim.tbl_keys(servers or {})
+		-- blacklist some servers
+		for blacklist_server, _ in ipairs(blacklist_lsp_servers) do
+			-- remove server from ensure_installed
+			print("Blacklisting server " .. blacklist_server)
+			ensure_installed[blacklist_server] = nil
+		end
+
+		-- install required tools
 		require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-		--
+		-- TODO: find a way to ensure that only ensure_installed stuff are installed, and 
+		-- other stuff is removed
+
 		require('mason-lspconfig').setup {
 			handlers = {
 				function(server_name)
--- nvim/lua/plugins/lsp/servers.lua
+++ nvim/lua/plugins/lsp/servers.lua
@@ -24,43 +24,43 @@ end
 
 return {
 	-- preludo buraz
-	-- basedpyright = {
-	-- 	-- https://docs.basedpyright.com/latest/configuration/language-server-settings/#based-settings
-	-- 	settings = {
-	-- 		basedpyright = {
-	-- 			disableOrganizedImports = true,
-	-- 			analysis = {
-	-- 				autoImportCompletions = true,
-	-- 				-- lets set this to false and see what happens,
-	-- 				autoSearchPaths = false,
-	-- 				-- we should define workspace in the basedpyright config
-	-- 				-- we dont have to set pythonPath because basedpyright is based
-	-- 				-- https://docs.basedpyright.com/latest/benefits-over-pyright/better-defaults/#default-value-for-pythonpath
-	--
-	-- 				-- i would want this to be "workspace" but basedpyright is too sluggish 
-	-- 				diagnosticMode = "openFilesOnly",
-	-- 				diagnosticSeverityOverrides = {
-	-- 					reportAny = false,
-	-- 					reportUnusedCallResult = false,
-	-- 					reportMissingTypeStubs = false,
-	-- 					reportUnknownMemberType = false,
-	-- 					reportUnknownVariableType = false,
-	-- 					reportUnknownArgumentType = false,
-	-- 					-- jel ti mene jebes 
-	-- 					reportMissingParameterType = false,
-	-- 					reportUnknownParameterType = false,
-	-- 					reportExplicitAny = false,
-	-- 					reportImplicitStringConcatenation = false,
-	-- 					reportMissingTypeArgument = false,
-	-- 				},
-	-- 				inlayHints = {
-	-- 					genericTypes = true,
-	-- 				}
-	-- 			}
-	-- 		}
-	-- 	},
-	-- 	root_dir = get_python_root_dir
-	-- },
+	basedpyright = {
+		-- https://docs.basedpyright.com/latest/configuration/language-server-settings/#based-settings
+		settings = {
+			basedpyright = {
+				disableOrganizedImports = true,
+				analysis = {
+					autoImportCompletions = true,
+					-- lets set this to false and see what happens,
+					autoSearchPaths = false,
+					-- we should define workspace in the basedpyright config
+					-- we dont have to set pythonPath because basedpyright is based
+					-- https://docs.basedpyright.com/latest/benefits-over-pyright/better-defaults/#default-value-for-pythonpath
+
+					-- i would want this to be "workspace" but basedpyright is too sluggish 
+					diagnosticMode = "openFilesOnly",
+					diagnosticSeverityOverrides = {
+						reportAny = false,
+						reportUnusedCallResult = false,
+						reportMissingTypeStubs = false,
+						reportUnknownMemberType = false,
+						reportUnknownVariableType = false,
+						reportUnknownArgumentType = false,
+						-- jel ti mene jebes 
+						reportMissingParameterType = false,
+						reportUnknownParameterType = false,
+						reportExplicitAny = false,
+						reportImplicitStringConcatenation = false,
+						reportMissingTypeArgument = false,
+					},
+					inlayHints = {
+						genericTypes = true,
+					}
+				}
+			}
+		},
+		root_dir = get_python_root_dir
+	},
 	lua_ls = {
 		capabilities = {},
 		settings = {
