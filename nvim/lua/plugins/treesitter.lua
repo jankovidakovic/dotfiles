e7b1f2d4783bf15bb1809e42591192e2b4d21d0b
jankovidakovic <janko@tensorpix.ai>
2025-10-13 15:32:55 +0200

    [nvim] [treesitter] Add javascript and json

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,23 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "lua", "vimdoc", "python", "markdown", "markdown_inline", "bash" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
+		configs.setup({
+			ensure_installed = {
+				"lua",
+				"vimdoc",
+				"python",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"javascript",
+				"json"
+			},
+			sync_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end
 }
