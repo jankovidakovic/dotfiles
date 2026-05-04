7dcc2aefa7b4a54a7beeeb11e34b321fabfa39ff
jvidakovic <janko.vidakovic@happening.xyz>
2026-05-04 23:23:04 +0200

    [nvim] update telescope and treesitter to nvim 0.12

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,5 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = {
-			"lua",
-			"vimdoc",
-			"python",
-			"markdown",
-			"markdown_inline",
-			"bash",
-			"javascript",
-			"typescript",
-			"html",
-			"css",
-			"scss",
-			"json",
-		},
-		sync_install = true,
-		highlight = { enable = true },
-		indent = { enable = true },
-	},
+	lazy = false
 }
