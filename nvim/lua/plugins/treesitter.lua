fdc9228d84dcc4d42be279eadb7dee8f2dd4ce60
jvidakovic <janko.vidakovic@happening.xyz>
2026-05-04 21:37:08 +0200

    wip [nvim] [lsp] attempt to fix telescope and treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,23 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-
-		configs.setup({
-			ensure_installed = {
-				"lua",
-				"vimdoc",
-				"python",
-				"markdown",
-				"markdown_inline",
-				"bash",
-				"javascript",
-				"typescript",
-				"html",
-				"css",
-				"scss",
-				"json"
-			},
-			sync_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end
+	opts = {
+		ensure_installed = {
+			"lua",
+			"vimdoc",
+			"python",
+			"markdown",
+			"markdown_inline",
+			"bash",
+			"javascript",
+			"typescript",
+			"html",
+			"css",
+			"scss",
+			"json",
+		},
+		sync_install = true,
+		highlight = { enable = true },
+		indent = { enable = true },
+	},
 }
