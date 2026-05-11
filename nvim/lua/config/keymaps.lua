5c24d94249fbf6be796c2dc2887c76eaf6e2a2dd
jvidakovic <janko.vidakovic@happening.xyz>
2026-05-11 12:29:57 +0200

    [nvim] remove <leader>q because <leader>sd does it already

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,7 +6,6 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('n', '<leader>vd', function()
 	vim.diagnostic.open_float { border = "rounded" }
 end, { desc = '[V]iew [D]iagnostics' })
